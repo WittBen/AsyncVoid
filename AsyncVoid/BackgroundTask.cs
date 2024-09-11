c49f9e24d67a8bae6f2477a5b953f5702b3c1ef9
WittBen <BenjaminWitt@web.de>
2024-09-11 18:03:36 +0200

    Add files via upload

---
+++ AsyncVoid/BackgroundTask.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1;
+
+public class BackgroundTask
+{
+  public async void RunAsync()
+  {
+      await Task.Delay(1000);
+      Console.WriteLine("wait for a seconds");
+
+
+    await  AnotherBackgroundTask();
+  }
+
+
+  private async Task AnotherBackgroundTask()
+  {
+    //do something...
+    await Task.Delay(1000);
+    try
+    {
+     // error occurs
+      int[] myNumbers = { 1, 2, 3 };
+      Console.WriteLine(myNumbers[10]); // error!
+    }
+    catch (Exception ex)
+    {
+       throw new Exception("ooops... an error");
+    }
+
+  }
+
+
+}
---
+++ AsyncVoid/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+
+var process = new BackgroundTask();
+
+Console.WriteLine("Start");
+
+try
+{
+  process.RunAsync();
+}
+catch(Exception ex)
+{
+  Console.WriteLine(ex.ToString()); 
+}
+
+Console.WriteLine("End");
+
+Console.ReadKey();
